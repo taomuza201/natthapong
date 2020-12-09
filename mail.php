a9d0da8e26761185e12a4a265a34161bbd20ebda
unknown <taomuza20@gmmail.com>
2020-12-09 17:37:21 +0700

    mail

---
+++ mail.php
@@ -0,0 +1,16 @@
+<?php
+
+
+$name = $_POST['name'];
+$email = $_POST['email'];
+$subject = $_POST['subject'];
+$message = $_POST['message'];
+// the message
+// $msg = "First line of text\nSecond line of text";
+
+// use wordwrap() if lines are longer than 70 characters
+// $msg = wordwrap($msg,70);
+
+// send email
+mail($email,$subject ,$messag);
+?>
